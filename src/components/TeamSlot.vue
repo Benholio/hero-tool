a6b71390e0d19e405ec24ed74b1c1f487b15216b
Ben Collier <bencollier78@gmail.com>
2020-07-25 14:02:56 -0400

    Generated @vue/cli + vuetify app skeleton

---
+++ src/components/TeamSlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>{{ name }}</span>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TeamSlot",
+  data: () => ({}),
+  props: {
+    id: { type: Number, required: true }
+  },
+  computed: {
+    ...mapGetters(["slotNames"]),
+    name() {
+      return this.slotNames[this.id];
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,22 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const state = {
+  slotNames: ["Left Wing", "Left Flank", "Tank", "Right Flank", "Right Wing"]
+};
+
+const getters = {
+  slotNames: state => state.slotNames
+};
+
+const actions = {};
+
+const mutations = {};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  getters,
+  actions,
+  mutations,
   modules: {}
 });
